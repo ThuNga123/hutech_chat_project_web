a09e1114afd7b1acb842fd837a4f1a6918df5b46
ThuNga123 <duongthithunga931@gmail.com>
2022-12-08 21:41:12 +0700

    nn

--- src/components/Admin/AddPatient.jsx
+++ src/components/Admin/AddPatient.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../styles/Admin/AddPatient.css'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Modal from 'react-bootstrap/Modal'
 
 function AddPatient({ setOpenModal }) {
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
   return (
     <div className="modalBackground">
       <div className="modalContainer">
@@ -14,6 +21,75 @@ function AddPatient({ setOpenModal }) {
             X
           </button>
         </div>
+        <form className="form">
+          <div className="tit">THÔNG TIN KHÁCH HÀNG</div>
+          <div className="row">
+            <div className="row-name">Họ và tên: </div>
+            <div className="row-input">
+              <input
+                className="input"
+                type="text"
+                id="text"
+                name="text"
+                placeholder="Vui lòng nhập họ tên"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="row-name">Giới tính: </div>
+            <div className="row-input">
+              <input
+                className="input"
+                type="text"
+                id="text"
+                name="text"
+                placeholder="Vui lòng nhập giới tính"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="row-name">Số điện thoại: </div>
+            <div className="row-input">
+              <input
+                className="input"
+                type="text"
+                id="text"
+                name="text"
+                placeholder="Vui lòng nhập số điện thoại"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="row-name">Email: </div>
+            <div className="row-input">
+              <input
+                className="input"
+                type="text"
+                id="text"
+                name="text"
+                placeholder="Vui lòng nhập email"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="row-name">Địa chỉ: </div>
+            <div className="row-input">
+              <input
+                className="input"
+                type="text"
+                id="text"
+                name="text"
+                placeholder="Vui lòng nhập địa chỉ"
+              />
+            </div>
+          </div>
+          <button className="btt-sub" type="submit">
+            Tạo mới
+          </button>
+          <button className="btt-can" type="cancel">
+            Huỷ bỏ
+          </button>
+        </form>
       </div>
     </div>
   )
--- src/components/Admin/AddPersonnel.jsx
+++ src/components/Admin/AddPersonnel.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../styles/Admin/AddPatient.css'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Modal from 'react-bootstrap/Modal'
 
 function AddPatient({ setOpenModal }) {
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
   return (
     <div className="modalBackground">
       <div className="modalContainer">
@@ -14,6 +21,87 @@ function AddPatient({ setOpenModal }) {
             X
           </button>
         </div>
+        <form className="form-per">
+          <div className="tit-per">THÔNG TIN NHÂN VIÊN</div>
+          <div className="row-per">
+            <div className="row-name-per">Họ và tên: </div>
+            <div className="row-input-per">
+              <input
+                className="input-per"
+                type="text"
+                id="text"
+                name="text"
+                placeholder="Vui lòng nhập họ tên"
+              />
+            </div>
+          </div>
+          <div className="row-per">
+            <div className="row-name-per">Giới tính: </div>
+            <div className="row-input-per">
+              <input
+                className="input-per"
+                type="text"
+                id="text"
+                name="text"
+                placeholder="Vui lòng nhập giới tính"
+              />
+            </div>
+          </div>
+          <div className="row-per">
+            <div className="row-name-per">Số điện thoại: </div>
+            <div className="row-input-per">
+              <input
+                className="input-per"
+                type="text"
+                id="text"
+                name="text"
+                placeholder="Vui lòng nhập số điện thoại"
+              />
+            </div>
+          </div>
+          <div className="row-per">
+            <div className="row-name-per">Email: </div>
+            <div className="row-input-per">
+              <input
+                className="input-per"
+                type="text"
+                id="text"
+                name="text"
+                placeholder="Vui lòng nhập email"
+              />
+            </div>
+          </div>
+          <div className="row-per">
+            <div className="row-name-per">Địa chỉ: </div>
+            <div className="row-input-per">
+              <input
+                className="input-per"
+                type="text"
+                id="text"
+                name="text"
+                placeholder="Vui lòng nhập địa chỉ"
+              />
+            </div>
+          </div>
+          <div className="row-per">
+            <div className="row-name-per">Chức vụ: </div>
+            <div className="row-input-per">
+              <input
+                className="input-per"
+                type="text"
+                id="text"
+                name="text"
+                placeholder="Vui lòng nhập chức vụ"
+              />
+            </div>
+          </div>
+          <button className="btt-sub-per" type="submit">
+            Tạo mới
+          </button>
+          <button className="btt-can-per" type="cancel">
+            Huỷ bỏ
+          </button>
+        </form>
       </div>
     </div>
   )
